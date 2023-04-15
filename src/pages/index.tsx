f939a30836c90357dbcaaa8a01542e67d9f6569a
roshanchatei <rkc.mj5@gmail.com>
2023-04-15 11:03:46 +0530

    home search deone

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,48 @@
-import {Box} from "@mui/material";
+import {Box, Container, TextField} from "@mui/material";
+
+import SearchIcon from "@mui/icons-material/Search";
+
 
 export default function Home() {
   return (
     <>
-      <Box>
-        Hello
-      </Box>
+        <Box
+            width={"100%"}
+            height={'100vh'}
+            sx={{
+                background: `url('/images/home-bg.jpg')`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                px: 2,
+                pt: 6,
+                pb: 3,
+            }}
+        >
+            <Container maxWidth={'md'}>
+                <TextField
+                    fullWidth
+                    sx={{
+                        background: "#FFF",
+                        py: 1.3,
+                        px: 2,
+                        mx: {xs: 0, md: 4},
+                        borderRadius: "10px",
+                        boxShadow: "0px 4px 31px rgba(0, 0, 0, 0.08)",
+                    }}
+                    variant="standard"
+                    size={"medium"}
+                    placeholder={"Search"}
+                    InputProps={{
+                        startAdornment: <SearchIcon sx={{mr: 2}} />,
+                        disableUnderline: true,
+                    }}
+                />
+            </Container>
+
+        </Box>
     </>
   )
 }
