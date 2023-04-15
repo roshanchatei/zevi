9f9606cb2e80f7c42123d1eab24ecc75af12a3af
roshanchatei <rkc.mj5@gmail.com>
2023-04-15 10:50:46 +0530

    changing app.js and adding notistack

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,40 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import {SnackbarProvider} from "notistack";
+import Head from "next/head";
+import {Box, CssBaseline} from "@mui/material";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <SnackbarProvider>
+          <Head>
+            <title>
+              {"Zevi"}
+            </title>
+            <meta
+                content="initial-scale=1, width=device-width"
+                name="viewport"
+            />
+          </Head>
+        </SnackbarProvider>
+          <ThemeProvider theme={createTheme({
+              palette: {
+                  primary: {
+                      main: '#319EFF',
+                  },
+                  secondary: {
+                      main: '#545454',
+                  },
+              },
+          })}>
+          <CssBaseline />
+          <Box width={'100%'} height={'100%'}>
+            <Component {...pageProps} />
+          </Box>
+        </ThemeProvider>
+
+      </>
+  )
 }
